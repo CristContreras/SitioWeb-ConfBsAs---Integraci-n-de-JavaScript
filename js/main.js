b9c11e52ed501bf6bee77e5827c5b3731df80711
CristContreras <cristian.g.contreras@gmail.com>
2023-11-03 02:56:11 -0300

    diseño ok, javascript ok, falta responsive

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const descuentoEstudiante = 80;
 const descuentoTrainee = 50;
 const descuentoJunior = 15;
 let precioFinal;
-let catSeleccionada;
+
 
 var elementPrecio = document.getElementById("totalPagar");
 
@@ -18,7 +18,7 @@ elementCantidad.addEventListener("input", function () {
         //muestra numero actual en consola
         // console.log(cantidadPersonas);
         catSeleccionada = elementCategoria.value;
-        if (catSeleccionada != "selección") {
+        if (catSeleccionada != "") {
             switch (catSeleccionada) {
                 case "estudiante":
                     precioFinal = calcularPrecio(cantidadPersonas, descuentoEstudiante, catSeleccionada)
@@ -26,6 +26,19 @@ elementCantidad.addEventListener("input", function () {
                     elementPrecio.innerHTML = " Total a Pagar: $" + precioFinal;
                     // console.log("Precio final: " + precioFinal);
                     break;
+                case "trainee":
+                    precioFinal = calcularPrecio(cantidadPersonas, descuentoTrainee, catSeleccionada)
+                    // alert("Precio final: " + precioFinal);
+                    elementPrecio.innerHTML = " Total a Pagar: $" + precioFinal;
+                    // console.log("Precio final: " + precioFinal);
+                    break;
+                case "junior":
+                    precioFinal = calcularPrecio(cantidadPersonas, descuentoJunior, catSeleccionada)
+                    // alert("Precio final: " + precioFinal);
+                    elementPrecio.innerHTML = " Total a Pagar: $" + precioFinal;
+                    // console.log("Precio final: " + precioFinal);
+                    break;
+
             }
         } else {
 
@@ -38,11 +51,29 @@ elementCantidad.addEventListener("input", function () {
                         // alert("Precio final: " + precioFinal);
                         elementPrecio.innerHTML = " Total a Pagar: $" + precioFinal;
                         // console.log("Precio final: " + precioFinal);
+                        descuentoAplicado = descuentoEstudiante;
+
+                        break;
+                    case "trainee":
+                        precioFinal = calcularPrecio(cantidadPersonas, descuentoTrainee, catSeleccionada)
+                        // alert("Precio final: " + precioFinal);
+                        elementPrecio.innerHTML = " Total a Pagar: $" + precioFinal;
+                        // console.log("Precio final: " + precioFinal);
+                        descuentoAplicado = descuentoTrainee;
+
+                        break;
+                    case "junior":
+                        precioFinal = calcularPrecio(cantidadPersonas, descuentoJunior, catSeleccionada)
+                        // alert("Precio final: " + precioFinal);
+                        elementPrecio.innerHTML = " Total a Pagar: $" + precioFinal;
+                        // console.log("Precio final: " + precioFinal);
+                        descuentoAplicado = descuentoJunior;
+
                         break;
                 }
             })
         }
-    }else if(cantidadPersonas==''){
+    } else if (cantidadPersonas == '') {
         elementPrecio.innerHTML = " Total a Pagar: $";
     }
 });
@@ -52,3 +83,75 @@ function calcularPrecio(cantPersonas, cantidadDescuento, catSeleccionada) {
     precioFinal = (valorTicket - descuento) * cantPersonas;
     return precioFinal;
 }
+
+const btn_borrar = document.getElementById("btn-borrar");
+
+function borrarFormulario() {
+    // const textoElemento = elementNombre.textContent;
+    elementNombre.textContent = "";
+    // alert("se hizo clikc en borrar");
+}
+
+btn_borrar.addEventListener("click", borrarFormulario);
+
+
+
+
+
+//Funcion boton resumen
+
+const btn_resumen = document.getElementById("btn-resumen");
+const miModal = document.getElementById("miModal");
+const cerrarModal = document.getElementById("cerrarModal");
+let elementNombre = document.getElementById("nombre");
+let elementApellido = document.getElementById("apellido");
+let elementEmail = document.getElementById("email");
+let resumenDetalle = document.getElementById("resumenDetalle");
+
+
+
+document.getElementById("formulario").addEventListener('submit', function(event){
+    event.preventDefault();
+})
+
+document.getElementById("btn-borrar").addEventListener('click', function(){
+    elementPrecio.textContent="Total a Pagar: $";
+})
+
+btn_resumen.addEventListener("click", function () {
+    if (elementNombre.value == '' || elementApellido.value == '' || elementEmail.value == '' || elementCantidad.value == '' || elementCategoria.value == '') {
+
+    } else {
+        miModal.style.display = "block";
+
+        resumenDetalle.innerHTML = '<span class="centrarTitulo" style="font-size: 20px"><b><u>Resumen de compra</u></b></span><br>' + 'Nombre: ' + elementNombre.value + "<br>" +
+            "Apellido: " + elementApellido.value + "<br>" +
+            "Email: " + elementEmail.value + "<br>" +
+            "Cantidad de personas: " + elementCantidad.value + "<br>" +
+            "Categoria: " + elementCategoria.value + "<br>" +
+            "Descuento aplicado: " + descuentoAplicado + "%" + "<br>" +
+            "-----------------------------------------------<br>" +
+            '<span style="font-size: 1.5em"><b>TOTAL A PAGAR: </b></span>' +
+            '<span style="font-size: 1.5em"><b>$</b></span>' +
+            '<span style="font-size: 1.5em"><b>' + precioFinal + '</b></span>';
+
+    }
+
+})
+
+cerrarModal.addEventListener("click", function () {
+    miModal.style.display = "none";
+});
+
+window.addEventListener("click", function (event) {
+    if (event.target === miModal) {
+        miModal.style.display = "none";
+    }
+});
+
+
+
+
+
+
+
